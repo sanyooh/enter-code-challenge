0d6f3e38b1c5398718b47f6d0a519e329396eac6
sandraoldenburg <sandra@spread.ai>
2023-04-29 12:40:42 +0200

    Implement basic file system types and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Box, Grid} from "@mui/material";
+
+interface File {
+    type: 'file';
+    name: string;
+}
+interface Directory {
+    type: 'directory';
+    name: string;
+    content?: (Directory | File)[];
+}
+
+type FileSystemEntry = Directory | File;
+
+const mockData: FileSystemEntry[] = [
+    {
+        type: 'directory',
+        name: 'level 1',
+        content: [
+            {
+                type: 'directory',
+                name: 'level 2',
+                content: [
+                    {
+                        type: 'directory',
+                        name: 'level 3',
+                    }
+                ]
+            },
+            {
+                type: 'file',
+                name: 'level 2',
+            }
+        ],
+    },
+    {
+        type: 'directory',
+        name: 'level 1',
+    },
+    {
+        type: 'file',
+        name: 'level 1',
+    }
+];
+
+interface FileSystemProps {
+    data: FileSystemEntry[];
+}
+
+
+const FileSystem: React.FC<FileSystemProps> = ({ data }) => {
+    const renderFileSystem = (entries: FileSystemEntry[]) => {
+        return entries.map((entry, index) => {
+            if (entry.type === 'directory') {
+                return (
+                    <Grid key={index} item>
+                        <Grid container direction="row" alignItems="start">
+                            <Box pr="30px">
+                                📁 {entry.name}
+                            </Box>
+                            {entry.content && (
+                                <Grid item>
+                                    <Grid container direction="column" spacing={2} alignItems="start">{renderFileSystem(entry.content)}</Grid>
+                                </Grid>
+                            )}
+                        </Grid>
+                    </Grid>
+                );
+            } else if (entry.type === 'file') {
+                return (
+                    <Grid key={index} item>
+                        <Grid container direction="row" alignItems="start">
+                            <Grid item>
+                                <Box>
+                                    📄 {entry.name}
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                );
+            }
+            return null;
+        });
+    };
+
+    return (
+        <Grid container direction="column" spacing={2}>
+            {renderFileSystem(data)}
+        </Grid>
+    );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p="20px">
+        <>
+            <h2>Enter Code Challenge - Finder</h2>
+            <FileSystem data={mockData} />
+        </>
+    </Box>
   );
 }
 
